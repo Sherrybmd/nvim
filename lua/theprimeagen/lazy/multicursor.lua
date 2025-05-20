347ca23d137094391b48d6aabe0976983128f38a
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-05-20 12:52:28 +0330

    forgor

--- lua/theprimeagen/lazy/multicursor.lua
+++ lua/theprimeagen/lazy/multicursor.lua
@@ -25,7 +25,8 @@ return {
         set("n", "<c-leftrelease>", mc.handleMouseRelease)
 
         -- Disable and enable cursors.
-        set({"n", "x"}, "<c-q>", mc.toggleCursor)
+        set({"n", "x"}, "<c-q>", mc.addCursor)
+        --set({"n", "x"}, "<c-q>", mc.toggleCursor)
 
         -- Mappings defined in a keymap layer only apply when there are
         -- multiple cursors. This lets you have overlapping mappings.
